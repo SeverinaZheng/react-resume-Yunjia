ae52551dee8df4e4f21586abc521fc973831f7b7
yzheng57 <yunjia.zheng@mail.mcgill.ca>
2025-06-26 09:21:31 -0400

    try another

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -5,7 +5,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER,
   port: Number(process.env.EMAIL_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
